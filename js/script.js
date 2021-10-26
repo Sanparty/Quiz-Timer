4b10eebaac1f8e311741fefc0ec66f2af2b2e2d5
Sanparty <88841545+Sanparty@users.noreply.github.com>
2021-10-26 15:41:01 -0400

    added end timer and hour glass animation

--- js/script.js
+++ js/script.js
@@ -42,7 +42,18 @@
 
 
 
-
+const finished  = () => {
+    document.getElementById("time_image_source").classList.remove("nodisplay");
+    document.getElementById("time_image_source").src="images/1465831_clock_time_alarm_calendar_timer__finish_icon.svg";
+    document.getElementById("time_image_source2").classList.add("nodisplay");
+    document.querySelector("#timeinput").classList.add("nodisplay");
+    document.querySelector("#timerstart").classList.add("nodisplay");
+    document.querySelector("#timeperquestion").classList.add("nodisplay");
+    document.getElementById("btn").classList.add("nodisplay");
+    let finished = document.getElementById("finished");
+    finished.innerHTML = `
+    <a href="index.html"><button id="newquiz">New Quiz</button></a>`;
+}
 
 
 
@@ -60,7 +71,7 @@
 
  // Populate form with an input and a button
  lengthTime.innerHTML = `
-    <h1>Dan's Quiz Timer</h1>
+    <div class="headtitle">Dan's Quiz Timer</div>
    <p id="quiz_intro">This timer will help you pace yourself during an exam, test, or quiz. Just fill in how much time is alloted for the exam and then how many questions you have to complete. Follow the countdown timer and also get an updated time for each question remaining that you have to complete.</p>
    <hr class="hr1"> 
    <input type="number" id="test_duration" name="test_duration" placeholder="Duration in minutes" required> 
@@ -78,8 +89,8 @@
    let newQuestions = lengthTime.querySelector("#test_questions").value;
   
    // Set the value of the field
-   document.querySelector("#timeinput").innerHTML = `<h1>Good Luck!</h1>` ;
-
+   document.querySelector("#timeinput").innerHTML = `<div class="headtitle">Good Luck!</div>` ;
+   document.getElementById("time_image_source").src="images/1465831_clock_time_alarm_calendar_timer__start_icon.svg";
    // Clear the form input
    lengthTime.querySelector("input").value = "";
    console.log(newDuration, newQuestions);
@@ -122,8 +133,10 @@ function startTimer(duration, display) {
         if (--timer < 0) {
             timer = duration;
         }
-    } else {
-        document.getElementById("timeinput").innerHTML = "Your time is up!";
+        } else {    
+        document.querySelector(".result").innerHTML = "<h1>Your time is up!</h1>";
+        finished();
+        return
     }
     }, 1000);
 }
@@ -133,6 +146,9 @@ function timerstartClick() {
     startTimer(fiveMinutes, display);
     document.getElementById("start").classList.add("nodisplay");
     document.getElementById("btn").classList.remove("nodisplay");
+    document.getElementById("time_image_source").classList.add("nodisplay");
+    document.getElementById("time_image_source2").innerHTML = `<svg height="100px" id="Слой_1" style="enable-background:new 0 0 48 48;" version="1.1" viewBox="0 0 48 48" width="100px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g><g><path d="M31,11c0,3-3.1,7-7,7s-7-4-7-7H31z" class="top-timer" style="fill:white;"/><path class="bottom-timer" d="M31,37c0-4-3.1-7-7-7s-7,3-7,7H31z" style="fill:none;"/></g><path d="M38,11v-1c0-1.1,0.9-2,2-2s2-0.9,2-2V2c0-1.1-0.9-2-2-2H8C6.9,0,6,0.9,6,2v4c0,1.1,0.9,2,2,2     s2,0.9,2,2v1c0,5.9,3.7,10.9,8.8,13c-5.2,2.1-8.8,7.1-8.8,13v1c0,1.1-0.9,2-2,2s-2,0.9-2,2v4c0,1.1,0.9,2,2,2h32c1.1,0,2-0.9,2-2     v-4c0-1.1-0.9-2-2-2s-2-0.9-2-2v-1c0-5.9-3.7-10.9-8.8-13C34.3,21.9,38,16.9,38,11z M8,2h32l0,4H8V2z M40,46H8v-4h32L40,46z      M36,37v1c0,0.7,0.2,1.4,0.6,2H11.4c0.3-0.6,0.6-1.3,0.6-2v-1c0-6.3,4.9-11.4,11-11.9V27h2v-1.9C31.1,25.6,36,30.7,36,37z      M25,22.9V21h-2v1.9c-6.1-0.5-11-5.7-11-11.9v-1c0-0.7-0.2-1.4-0.6-2h25.1C36.2,8.6,36,9.3,36,10v1C36,17.3,31.1,22.4,25,22.9z" style="fill:#303033;"/></g></g></svg>`;
+
     // let endquiz = document.createElement("div");
     // endquiz.classList.add(`endquiz`);
     // endquiz.innerHTML = `
@@ -161,17 +177,9 @@ function buttonClick() {
     <p>Remaining time per question: ${questionsleftproper}</p>`;
     } else {
         document.querySelector(".result").innerHTML = `
-        <h1>Congrats on finishing the test!</h1>`;
-        document.querySelector("#timeinput").classList.add("nodisplay");
-        document.querySelector("#timerstart").classList.add("nodisplay");
-        document.querySelector("#timeperquestion").classList.add("nodisplay");
-        document.getElementById("btn").classList.add("nodisplay");
-        let finished = document.createElement("div");
-        finished.classList.add(`finished`);
-        finished.innerHTML = `
-        <a href="index.html"><button id="newquiz">New Quiz</button></a>`;
-        addbutton = document.querySelector("#qleft");
-        addbutton.append(finished);
+        <h1>Congrats on finishing the quiz!</h1>`;
+       finished();
+       return;
     }
 }
 
@@ -204,7 +212,7 @@ document.getElementById("timeperquestion").innerHTML = `
 ;}
 
 document.getElementById("newduration").innerHTML = `
-<h1>Duration of test: ${t} minutes</h1>`;
+<h1>Duration of quiz: ${t} minutes</h1>`;
 
 
 
